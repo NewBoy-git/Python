8aabdc36eeceb857b08a2c29d0170d89def5fe86
NewBoy-git <1435526402@qq.com>
2019-10-14 15:37:00 +0800

    testsitecrawl

---
+++ testSiteCrawlApi/MysqlHandler/status_table.py
@@ -0,0 +1,28 @@
+import pymysql
+
+def update_status(userid,company,status):
+    connect = pymysql.connect(
+        host='47.110.49.102',
+        db='credit_system_zhiguo',
+        user='hyj',
+        passwd='Hyjkj@1009',
+        charset='utf8',
+        use_unicode=True
+    )
+    cursor = connect.cursor()
+    company_map = {
+        "AN_NENG":"anneng_tms_state",
+        "YI_MI_DI_DA":"yimi_tms_state",
+        "BAI_SHI":"baishi_tms_state",
+        "SHUN_XIN_JIE_DA":"shunxin_tms_state"
+    }
+    print(userid,company_map[company],status)
+
+    sql = "UPDATE upgrade_quota set {}='{}' WHERE user_id ={};".format(company_map[company],status,userid)
+    print(sql)
+
+    cursor.execute(sql)
+    connect.commit()
+    cursor.close()
+    
+    
